8ce8cd59d351bfd01156c093f2b9e482d84828e4
MrDrprofessorGuy <guycohen801@gmail.com>
2022-06-25 17:19:44 +0300

    a

--- test_src_files/test10_cmdline_args.c
+++ test_src_files/test10_cmdline_args.c
@@ -1,6 +1,7 @@
-
+#include "stdio.h"
 int main(int argc, char** argv)
 {
+  
   if(argc != 5)
     return 1;
   if(*(*(argv+1)) == 'D')
@@ -9,6 +10,7 @@ int main(int argc, char** argv)
   }
   for(int i = 0; i < argc; i++)
   {
+      printf("arg[%d]: %d\n",i, (*(argv+i));
     bar(*(*(argv+i) + i));
   }
   return 0;
