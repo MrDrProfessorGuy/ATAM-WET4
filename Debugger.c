5d793afa83e0391e7ce92067a1070880174300ca
MrDrprofessorGuy <guycohen801@gmail.com>
2022-06-25 16:28:33 +0300

    a

--- Debugger.c
+++ Debugger.c
@@ -34,12 +34,14 @@ pid_t run_target(const char* program_name, char* program_arguments){
 
 unsigned long AddBreakpoint(Elf64_Addr address){
     unsigned long data = ptrace(PTRACE_PEEKTEXT, program_pid, (void*)address, NULL);
-    //printf("DBG: Original data at 0x%llx: 0x%lx\n", address, data);
+    
     
     unsigned long data_trap = (data & 0xFFFFFFFFFFFFFF00) | 0xCC;
+    printf("DBG: Original data at 0x%llx: 0x%lx,    trap: 0x%lx\n", address, data, data_trap);
     ptrace(PTRACE_POKETEXT, program_pid, (void*)address, (void*)data_trap);
     
     unsigned long data2 = ptrace(PTRACE_PEEKTEXT, program_pid, (void*)address, NULL);
+    printf("DBG: altered data at 0x%llx: 0x%lx\n", address, data2);
     assert(data2 == data_trap);
     
     return data;
