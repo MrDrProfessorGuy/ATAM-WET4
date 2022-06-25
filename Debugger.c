7a465fc2156b5cc243565d43751514fb9393158a
MrDrprofessorGuy <guycohen801@gmail.com>
2022-06-25 17:25:51 +0300

    a

--- Debugger.c
+++ Debugger.c
@@ -27,7 +27,7 @@ pid_t run_target(const char* program_name, char* program_arguments[]){
         if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) < 0){
             exit(1);
         }
-        execl(program_name, program_name, program_arguments, NULL);
+        execl(program_name, {program_name, program_arguments, NULL});
     }
     
     return FORK_ERROR;
